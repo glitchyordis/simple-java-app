212e084a39dbf64bcd9cc5696b55bd9a61e08675
Felix Kong <99534415+glitchyordis@users.noreply.github.com>
2025-04-10 14:51:48 +0800

    updated

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins upod o boring");
+        System.out.println("Jenkins o boring");
     }
 
     public String getMessage() {
