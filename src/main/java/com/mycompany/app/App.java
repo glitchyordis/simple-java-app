3c014f0f545d9f92d7bca75185eb867449bfae53
Felix Kong <99534415+glitchyordis@users.noreply.github.com>
2025-04-10 09:50:48 +0800

    some change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkins is 123 o boring");
+        System.out.println("Jenkins upod o boring");
     }
 
     public String getMessage() {
